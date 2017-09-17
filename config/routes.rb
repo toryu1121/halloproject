d04f903694a1cf24b051ce3171f5a1d560d179fb
toryu1121 <y28bdgw9@gmail.com>
2017-09-17 16:21:28 +0000

    hallo_initialize2

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get 'hallo/update'
 
   get 'hallo/destroy'
+  
+  root to: "hallo#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
